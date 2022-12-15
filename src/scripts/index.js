a396d19977faea8ae8cc72dbb58cc8dbab0ee2f4
Rihan soares <rihan145@gmail.com>
2022-12-15 17:03:38 -0300

    finalizando entrega

---
+++ src/scripts/index.js
@@ -0,0 +1,38 @@
+function renderLists() {
+
+}
+
+function createLists(jobs) {
+
+}
+
+createLists(jobsData)
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <li class="list__item--container">
+<h2 class="item__title">Pessoa desenvolvedora Front-end - React</h2>
+<div class="item__city--container">
+    <span>Kenzie Academy</span>
+    <span>Curitiba</span>
+</div>
+<p class="item__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, reprehenderit
+    accusantium excepturi
+    culpa, eum nihil doloribus esse neque quae nemo ea praesentium cupiditate maiores! Molestiae, impedit
+    sint!
+    Corrupti, maxime quia!</p>
+<div class="item__tag--container">
+    <span>Home Office</span>
+    <span>Hibrido</span>
+</div>
+<button class="item__button">Candidatar</button>
+</li> */}
